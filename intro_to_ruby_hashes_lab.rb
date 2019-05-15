45deba8ce77e8767728ac558e5d85d6ed227f9ce
Margot Konig <margot.konig@blackrock.com>
2019-05-15 18:53:34 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -8,7 +8,7 @@ actor = {name: 'Dwayne The Rock Johnson'}
 end
 
 def monopoly
-	monopoly = {railroads => {}
+	monopoly = {railroads: => {}
 	  
 	}
 
@@ -16,7 +16,11 @@ end
 
 def monopoly_with_second_tier
 	#copy and paste the monopoly hash you built in the previous method. We're going to add stuff to it here!
-
+monopoly = {railroads: => {
+  
+}
+	  
+	}
 end
 
 def monopoly_with_third_tier
