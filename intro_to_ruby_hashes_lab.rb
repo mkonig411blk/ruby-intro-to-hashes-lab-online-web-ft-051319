fecf7c7a2d4bb92bcc89098845ff92653bcee7ec
Margot Konig <margot.konig@blackrock.com>
2019-05-15 19:01:23 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -37,6 +37,10 @@ def monopoly_with_third_tier
 monopoly = {:railroads => {
               :pieces=> 4,
               :names=> {
+                :reading_railroad=> {},
+                :pennsylvania_railroad=> {},
+                :b_and_o_railroad=> {},
+                :shortline=> {}
                 
               },
               :rent_in_dollars=> {
