82050ee04de9c16ea4df97930c2ac08774fafa35
Margot Konig <margot.konig@blackrock.com>
2019-05-15 18:52:09 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,13 +1,16 @@
 def new_hash
-
+new_hash = {}
+new_hash
 end
 
 def actor
-
+actor = {name: 'Dwayne The Rock Johnson'}
 end
 
 def monopoly
-	monopoly = {}
+	monopoly = {railroads => {}
+	  
+	}
 
 end
 
